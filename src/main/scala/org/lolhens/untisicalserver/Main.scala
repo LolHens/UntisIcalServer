114ae1cc08f8eed9e3a8a0ffbb2fcd1e60fcbaa4
LolHens <pierrekisters@gmail.com>
2016-08-31 14:09:15 +0200

    finished ICalServer; project is now working

--- src/main/scala/org/lolhens/untisicalserver/Main.scala
+++ src/main/scala/org/lolhens/untisicalserver/Main.scala
@@ -1,5 +1,6 @@
 package org.lolhens.untisicalserver
 
+import org.lolhens.untisicalserver.http.server.ICalServer
 import org.lolhens.untisicalserver.ical.CachedICalProvider
 
 import scala.concurrent.duration._
@@ -11,11 +12,11 @@ import scala.language.postfixOps
   */
 object Main {
   val nixdorfFs15b = SchoolClass("nixdorf_bk_essen", "FS-15B", 183)
+  val iCalProvider = new CachedICalProvider(nixdorfFs15b, 1 minute)
+  val iCalServer = new ICalServer(iCalProvider)
 
   def main(args: Array[String]): Unit = {
-    val calendar = new CachedICalProvider(nixdorfFs15b, 1 minute)()
-
-    println(calendar)
+    iCalServer.start()
   }
 }
 
--- src/main/scala/org/lolhens/untisicalserver/http/server/ICalServer.scala
+++ src/main/scala/org/lolhens/untisicalserver/http/server/ICalServer.scala
@@ -1,30 +1,39 @@
 package org.lolhens.untisicalserver.http.server
 
+import java.nio.charset.StandardCharsets
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri, _}
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import org.lolhens.untisicalserver.ical.ICalProvider
 
 import scala.concurrent.Future
 
 /**
   * Created by pierr on 31.08.2016.
   */
-class ICalServer {
-def test = {
-  implicit val system = ActorSystem()
-  implicit val materializer = ActorMaterializer()
-  implicit val executionContext = system.dispatcher
+class ICalServer(val iCalProvider: ICalProvider) {
+  def start() = {
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
 
-  val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
-    Http().bind(interface = "localhost", port = 8080)
+    val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
+      Http().bind(interface = "localhost", port = 8080)
 
-  val bindingFuture: Future[Http.ServerBinding] =
-    serverSource.to(Sink.foreach { connection =>
-      //connection.handleWith()
-      // foreach materializes the source
-      println("Accepted new connection from " + connection.remoteAddress)
-      // ... and then actually handle the connection
-    }).run()
-}
+    val bindingFuture: Future[Http.ServerBinding] =
+      serverSource.to(Sink.foreach { connection =>
+        connection.handleWith(Flow[HttpRequest].collect {
+          case HttpRequest(GET, Uri.Path("/ical/fs15b"), _, _, _) =>
+            HttpResponse(entity = HttpEntity(ContentTypes.`application/octet-stream`, iCalProvider().toString.getBytes(StandardCharsets.UTF_8)))
+
+          case r: HttpRequest =>
+            r.discardEntityBytes() // important to drain incoming HTTP Entity stream
+            HttpResponse(404, entity = "Unknown resource!")
+        })
+      }).run()
+  }
 }
