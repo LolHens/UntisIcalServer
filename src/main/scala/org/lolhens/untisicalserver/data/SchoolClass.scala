4348481ef5c233c2e791f3dc97eb98c5ccd86bbc
Pierre Kisters <pierrekisters@gmail.com>
2016-09-05 23:21:07 +0200

    added fs16b

--- src/main/scala/org/lolhens/untisicalserver/data/SchoolClass.scala
+++ src/main/scala/org/lolhens/untisicalserver/data/SchoolClass.scala
@@ -24,21 +24,32 @@ case class SchoolClass(school: String,
 object SchoolClass {
   val classes = Map(
     "fs15b" ->
-      SchoolClass("nixdorf_bk_essen", "FS-15B", 183, Map(
-        "feh" -> "Herr Fehlen",
-        "krg" -> "Herr Krieg",
-        "kri" -> "Frau Krigel",
-        "meh" -> "Herr Mehl",
-        "bol" -> "Herr Boll",
-        "abl" -> "Frau Abel",
-        "dib" -> "Frau Diblik",
-        "win" -> "Frau Wingold"
-      ), Map(
+      SchoolClass("nixdorf_bk_essen", "FS-15B", 183, hnbkTeachers, Map(
         "it1" -> "Linux",
         "it2" -> "Cisco Praxis",
         "it3" -> "Theorie",
         "aw1" -> "Programmieren",
         "aw2" -> "Datenbanken"
-      ))
+      )),
+
+    "fs16b" ->
+      SchoolClass("nixdorf_bk_essen", "FS-16B", 187, hnbkTeachers)
+  )
+
+  val hnbkTeachers = Map(
+    "feh" -> "Herr Fehlen",
+    "krg" -> "Herr Krieg",
+    "kri" -> "Frau Krigel",
+    "meh" -> "Herr Mehl",
+    "bol" -> "Herr Boll",
+    "abl" -> "Frau Abel",
+    "dib" -> "Frau Diblik",
+    "win" -> "Frau Wingold",
+    "scm" -> "Herr Schmidt",
+    "fel" -> "Frau Feller",
+    "sci" -> "Herr Schiffers",
+    "wor" -> "Frau Worat",
+    "tri" -> "Frau Triebert Schreyer",
+    "wen" -> "Frau Went"
   )
 }
